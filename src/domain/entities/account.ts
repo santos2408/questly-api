7e26354c08a04edeb6b7de5029eafebd56678be2
Roger Santos <roger.santos36@gmail.com>
2026-03-09 18:08:43 -0300

    feat: add Account entity

---
+++ src/domain/entities/account.ts
@@ -0,0 +1,29 @@
+export type AccountProperties = {
+  email: string;
+  password: string;
+  username: string;
+  bio?: string | null;
+  createdAt?: Date;
+};
+
+export class Account {
+  private readonly accountProps: AccountProperties;
+
+  constructor(props: AccountProperties) {
+    this.accountProps = { ...props };
+    this.bio = props.bio ?? null;
+    this.createdAt = this.createdAt ?? new Date();
+  }
+
+  public get props(): AccountProperties {
+    return this.accountProps;
+  }
+
+  private set bio(value: string | null) {
+    this.accountProps.bio = value;
+  }
+
+  private set createdAt(value: Date) {
+    this.accountProps.createdAt = value;
+  }
+}
