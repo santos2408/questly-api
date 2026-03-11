51708b26b635e23330060784e8868d615d8b9eaa
Roger Santos <roger.santos36@gmail.com>
2026-03-11 18:40:51 -0300

    chore: add vitest coverage and jiti library

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    include: ["./src/**/*.{spec, test}.ts"],
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      include: ["./src/**/*.ts"],
+      exclude: ["./src/**/*.{spec, test}.ts"],
+    },
     // globals: true,
   },
 });
