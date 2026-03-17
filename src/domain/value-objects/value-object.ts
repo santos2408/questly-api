d86d785c52088a6c3c39c8965f94a187e93a0017
Roger Santos <roger.santos36@gmail.com>
2026-03-17 18:02:55 -0300

    feat: ensure ValueObject set a value correctly

---
+++ src/domain/value-objects/value-object.ts
@@ -0,0 +1,11 @@
+export class ValueObject<Value = any> {
+  protected _value: Value;
+
+  constructor(value: Value) {
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+}
