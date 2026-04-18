e5df24e2554e8b2b2b4344aab5082f00da1bd56f
Roger Santos <roger.santos36@gmail.com>
2026-04-18 15:26:16 -0300

    refactor: add some tasks on email-value-object file

--- src/domain/value-objects/email-value-object.ts
+++ src/domain/value-objects/email-value-object.ts
@@ -3,6 +3,11 @@ import { InvalidEmailError } from "../errors/invalid-email-error";
 
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+/**
+ * [] - a parte local, antes do @, pode ter até 64 caracteres
+ * [] - o domínio pode chegar perto de 255 caracteres em teoria
+ */
+
 export class Email extends ValueObject<string> {
   constructor(email: string) {
     super(email);
