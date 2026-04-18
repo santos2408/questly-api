23ada4604f680346ed140f04c50898924013ef07
Roger Santos <roger.santos36@gmail.com>
2026-04-18 13:19:00 -0300

    refactor: adjust InvalidEmailError default message

--- src/domain/errors/invalid-email-error.ts
+++ src/domain/errors/invalid-email-error.ts
@@ -1,5 +1,5 @@
 export class InvalidEmailError extends Error {
-  constructor(message = "e-mail should be valid.") {
+  constructor(message = "E-mail should be valid.") {
     super(message);
     this.name = "InvalidEmailError";
   }
