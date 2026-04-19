b2cbe494feb39ccc145b00a2e82028903af2300b
Roger Santos <roger.santos36@gmail.com>
2026-04-19 12:51:04 -0300

    test: ensure UniqueEntityId accept an 'uuid' passed in constructor

--- src/domain/value-objects/__tests__/unique-entity-id-value-object.spec.ts
+++ src/domain/value-objects/__tests__/unique-entity-id-value-object.spec.ts
@@ -1,23 +1,29 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
+import { faker } from "@faker-js/faker";
 import { UniqueEntityId } from "../unique-entity-id-value-object";
 import { InvalidUuidError } from "../../errors/invalid-uuid-error";
 
 describe("UniqueEntityIdValueObject Unit Tests", () => {
-  it("should throw error when uuid is invalid", () => {
+  it("should throw error when 'uuid' is invalid", () => {
     // arrange
     const id = "invalid_id";
     const expected = new InvalidUuidError();
-    const validateSpy = vi.spyOn(UniqueEntityId.prototype as any, "validate");
 
     // act
     const actual = () => new UniqueEntityId(id);
 
     // assert
     expect(actual).toThrowError(expected);
-    expect(validateSpy).toHaveBeenCalled();
   });
 
-  // it("should accept an uuid passed in constructor", () => {
-  //   //
-  // });
+  it("should accept an 'uuid' passed in constructor", () => {
+    // arrange
+    const id = faker.string.uuid();
+
+    // act
+    const actual = () => new UniqueEntityId(id);
+
+    // assert
+    expect(actual).not.toThrow();
+  });
 });
