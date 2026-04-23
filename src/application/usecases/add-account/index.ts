3c78b1802f763416b55331d6793b948ee45a59d5
Roger Santos <roger.santos36@gmail.com>
2026-04-23 18:30:15 -0300

    refactor: export add-account files from a single index file

---
+++ src/application/usecases/add-account/index.ts
@@ -0,0 +1,4 @@
+export type * from "./add-account";
+export type * from "./add-account-output";
+export type * from "./create-account-dto";
+export * from "./add-account-usecase";
--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,10 +1,8 @@
 import type { EmailValidator } from "../../protocols/email-validator";
-import type { AddAccount } from "../../../application/usecases/add-account/add-account";
-import type { CreateAccountDTO } from "../../../application/usecases/add-account/create-account-dto";
-import type { AddAccountOutput } from "../../../application/usecases/add-account/add-account-output";
+import type { AddAccount, CreateAccountDTO, AddAccountOutput } from "../../../application/usecases/add-account";
 import { ROLES, STATUS } from "../../../domain/enums";
-import { SignUpController } from "./signup-controller";
 import { MissingParamError, InvalidParamError, ServerError } from "../../errors";
+import { SignUpController } from "./signup-controller";
 
 interface SutTypes {
   sut: SignUpController;
