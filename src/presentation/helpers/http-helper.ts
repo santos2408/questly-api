e94f9e270b54848c40f15316943dfe2d4f99fed5
Roger Santos <roger.santos36@gmail.com>
2026-04-24 17:48:40 -0300

    refactor: simplify import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 import type { HttpResponse } from "../protocols/http";
-import { ServerError } from "../errors/server-error";
+import { ServerError } from "../errors";
 
 export const created = (data: any): HttpResponse => ({
   statusCode: 204,
