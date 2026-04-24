78c89922173fa3b6381a00c8378e8ec8d2195e7a
Roger Santos <roger.santos36@gmail.com>
2026-04-24 18:39:58 -0300

    refactor: use mocked validator method

--- src/presentation/utils/__tests__/email-validator-adapter.spec.ts
+++ src/presentation/utils/__tests__/email-validator-adapter.spec.ts
@@ -1,7 +1,15 @@
+import validator from "validator";
 import { EmailValidatorAdapter } from "../email-validator-adapter";
+
+vi.mock("validator", () => ({
+  default: {
+    isEmail: vi.fn(),
+  },
+}));
 
 describe("EmailValidator Adapter", () => {
   it("should return 'false' if validator returns 'false'", () => {
+    vi.mocked(validator.isEmail).mockReturnValueOnce(false);
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
