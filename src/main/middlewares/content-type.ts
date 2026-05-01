20b2def5ad3cb11375c0f50a5dbec67cb008cbfa
Roger Santos <roger.santos36@gmail.com>
2026-05-01 13:45:22 -0300

    feat: ensure content-type return any type when type is forced

---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,6 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const contentType = (req: Request, res: Response, next: NextFunction) => {
+  res.type("json");
+  next();
+};
