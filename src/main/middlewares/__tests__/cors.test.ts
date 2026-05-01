4b19aa3281257654f83aefead291120a314e356e
Roger Santos <roger.santos36@gmail.com>
2026-05-01 13:33:49 -0300

    test: ensure cors is enable

---
+++ src/main/middlewares/__tests__/cors.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import app from "../../config/app";
+
+describe("CORS Middleware", () => {
+  it("should enable CORS", async () => {
+    app.get("/test_cors", (req, res) => {
+      res.send();
+    });
+
+    // TODO: verificar liberação do CORS para que seja apenas para o domínio da aplicação front-end
+    await request(app)
+      .get("/test_cors")
+      .expect("access-control-allow-origin", "*")
+      .expect("access-control-allow-methods", "*")
+      .expect("access-control-allow-headers", "*");
+  });
+});
