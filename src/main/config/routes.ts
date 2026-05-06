f76e417fb160598e1f91d3b630f12adef889a3f5
Roger Santos <roger.santos36@gmail.com>
2026-05-06 14:12:38 -0300

    refactor: improve file read code

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,13 +1,16 @@
 import { Router, type Express } from "express";
 import fg from "fast-glob";
+import { dirname, join } from "node:path";
+import { fileURLToPath, pathToFileURL } from "node:url";
 
 export default async (app: Express): Promise<void> => {
   const router = Router();
-  const currentDirectoryProcess = process.cwd();
-  const files = fg.sync("**/src/main/routes/*.route.ts");
+  const currentDirectory = dirname(fileURLToPath(import.meta.url));
+  const routesDirectory = join(currentDirectory, "../routes");
+  const files = fg.sync("*.route.{ts,js}", { cwd: routesDirectory, absolute: true });
 
   const promises = files.map(async (file) => {
-    const route = (await import(`${currentDirectoryProcess}/${file}`)).default;
+    const route = (await import(pathToFileURL(file).href)).default;
     route(router);
   });
 
