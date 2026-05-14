55c67dae63eef1f685489b5cc0681af655246c0f
Roger Santos <roger.santos36@gmail.com>
2026-05-14 18:06:05 -0300

    feat: add LogControllerDecorator in SignUpController

--- src/main/factories/signup-controller-factory.ts
+++ src/main/factories/signup-controller-factory.ts
@@ -1,14 +1,17 @@
+import type { Controller } from "../../presentation/protocols/controller.js";
 import { SignUpController } from "../../presentation/controllers/signup/signup-controller.js";
 import { EmailValidatorAdapter } from "../../presentation/utils/email-validator-adapter.js";
 import { DbAddAccountUseCase } from "../../application/usecases/add-account/index.js";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter.js";
 import { AddAccountPostgresRepository } from "../../infra/database/postgres/repositories/postgres-account-repository.js";
+import { LogControllerDecorator } from "../decorators/log-decorator.js";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   const emailValidator = new EmailValidatorAdapter();
   const bcryptAdapter = new BcryptAdapter(salt);
   const addAccountPostgresRepository = new AddAccountPostgresRepository();
   const dbAddAccountUseCase = new DbAddAccountUseCase(bcryptAdapter, addAccountPostgresRepository);
-  return new SignUpController(emailValidator, dbAddAccountUseCase);
+  const signupController = new SignUpController(emailValidator, dbAddAccountUseCase);
+  return new LogControllerDecorator(signupController);
 };
